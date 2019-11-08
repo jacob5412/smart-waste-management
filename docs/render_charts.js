95d8cd6e11c43f776bd2d616db141c4d7a318cec
Jacob <kapsjacob@gmail.com>
2019-11-08 10:57:06 +0530

    initial commit

---
+++ docs/render_charts.js
@@ -0,0 +1,234 @@
+var level = [];
+var temps = [];
+var hums = [];
+var dataSeries = []
+// display all bins
+smartbins.once("value").then(function (snapshot) {
+    // Get the bin data from the most recent snapshot of data
+    // added to the bin list in Firebase
+    snapshot.forEach(function (childSnapshot) {
+        smartbin = childSnapshot.val();
+        dataSeries.push({
+            "date": smartbin.timestamp,
+            "fill": smartbin.filllevel,
+            "temp": smartbin.temp,
+            "hum": smartbin.hum
+        }, )
+    })
+    var spikes = [5, -5, 3, -3, 8, -8]
+    for (var i = 0; i < dataSeries.length; i++) {
+        var innerArr = [dataSeries[i].date, dataSeries[i].fill];
+        level.push(innerArr);
+        innerArr = [dataSeries[i].date, dataSeries[i].temp];
+        temps.push(innerArr);
+        innerArr = [dataSeries[i].date, dataSeries[i].hum];
+        hums.push(innerArr);
+    }
+
+    var options = {
+        chart: {
+            type: 'area',
+            stacked: false,
+            height: 350,
+            zoom: {
+                type: 'x',
+                enabled: true,
+                autoScaleYaxis: true
+            },
+            toolbar: {
+                autoSelected: 'zoom'
+            }
+        },
+        dataLabels: {
+            enabled: false
+        },
+        series: [{
+            name: 'Fill Level',
+            data: level
+        }],
+        markers: {
+            size: 0,
+        },
+        title: {
+            text: 'Garbage in bin',
+            align: 'left'
+        },
+        fill: {
+            type: 'gradient',
+            gradient: {
+                shadeIntensity: 1,
+                inverseColors: false,
+                opacityFrom: 0.5,
+                opacityTo: 0,
+                stops: [0, 90, 100]
+            },
+        },
+        yaxis: {
+            labels: {
+                formatter: function (val) {
+                    return (val).toFixed(0);
+                },
+            },
+            title: {
+                text: 'Level (cms)'
+            },
+        },
+        xaxis: {
+            type: 'datetime',
+        },
+
+        tooltip: {
+            shared: false,
+            y: {
+                formatter: function (val) {
+                    return (val);
+                }
+            }
+        }
+    }
+
+    var fillchart = new ApexCharts(
+        document.querySelector("#fillchart"),
+        options
+    );
+
+    fillchart.render();
+
+    var options2 = {
+        chart: {
+            type: 'area',
+            stacked: false,
+            height: 350,
+            zoom: {
+                type: 'x',
+                enabled: true,
+                autoScaleYaxis: true
+            },
+            toolbar: {
+                autoSelected: 'zoom'
+            }
+        },
+        dataLabels: {
+            enabled: false
+        },
+        series: [{
+            name: 'Temperature',
+            data: temps
+        }],
+        markers: {
+            size: 0,
+        },
+        title: {
+            text: 'Temperature in bin',
+            align: 'left'
+        },
+        fill: {
+            type: 'gradient',
+            gradient: {
+                shadeIntensity: 1,
+                inverseColors: false,
+                opacityFrom: 0.5,
+                opacityTo: 0,
+                stops: [0, 90, 100]
+            },
+        },
+        yaxis: {
+            labels: {
+                formatter: function (val) {
+                    return (val).toFixed(0);
+                },
+            },
+            title: {
+                text: 'Temperature (*C)'
+            },
+        },
+        xaxis: {
+            type: 'datetime',
+        },
+
+        tooltip: {
+            shared: false,
+            y: {
+                formatter: function (val) {
+                    return (val);
+                }
+            }
+        }
+    }
+
+    var tempchart = new ApexCharts(
+        document.querySelector("#tempchart"),
+        options2
+    );
+
+    tempchart.render();
+
+    var options3 = {
+        chart: {
+            type: 'area',
+            stacked: false,
+            height: 350,
+            zoom: {
+                type: 'x',
+                enabled: true,
+                autoScaleYaxis: true
+            },
+            toolbar: {
+                autoSelected: 'zoom'
+            }
+        },
+        dataLabels: {
+            enabled: false
+        },
+        series: [{
+            name: 'Humidity',
+            data: hums
+        }],
+        markers: {
+            size: 0,
+        },
+        title: {
+            text: 'Humidity in bin',
+            align: 'left'
+        },
+        fill: {
+            type: 'gradient',
+            gradient: {
+                shadeIntensity: 1,
+                inverseColors: false,
+                opacityFrom: 0.5,
+                opacityTo: 0,
+                stops: [0, 90, 100]
+            },
+        },
+        yaxis: {
+            labels: {
+                formatter: function (val) {
+                    return (val).toFixed(0);
+                },
+            },
+            title: {
+                text: 'Humidity (%)'
+            },
+        },
+        xaxis: {
+            type: 'datetime',
+        },
+
+        tooltip: {
+            shared: false,
+            y: {
+                formatter: function (val) {
+                    return (val);
+                }
+            }
+        }
+    }
+
+    var humchart = new ApexCharts(
+        document.querySelector("#humchart"),
+        options3
+    );
+
+    humchart.render();
+});
---
+++ docs/script.js
@@ -0,0 +1,84 @@
+// Your web app's Firebase configuration
+let firebaseConfig = {
+    apiKey: "AIzaSyBiypk00NLUuR0D9x5-SU8jYVfMf35gBq4",
+    authDomain: "iot-smart-bin-3ca3d.firebaseapp.com",
+    databaseURL: "https://iot-smart-bin-3ca3d.firebaseio.com",
+    projectId: "iot-smart-bin-3ca3d",
+    storageBucket: "iot-smart-bin-3ca3d.appspot.com",
+    messagingSenderId: "587124346578",
+    appId: "1:587124346578:web:714ca2432fb8c06f67670b",
+    measurementId: "G-41RTMWV35Q"
+};
+firebase.initializeApp(firebaseConfig);
+
+// Get a reference to the database service
+let smartbins = firebase.database().ref('smartbin');
+
+// display all bins
+smartbins.once("value").then(function (snapshot) {
+    // Get the bin data from the most recent snapshot of data
+    // added to the bin list in Firebase
+    snapshot.forEach(function (childSnapshot) {
+        smartbin = childSnapshot.val();
+        // Update the HTML to display the bin text
+        $("#displayTable").append("<tr><td>" 
+        + smartbin.id + "</td><td>" 
+        + smartbin.timestamp + "</td><td>" 
+        + smartbin.filllevel + "</td><td>" 
+        + smartbin.temp + "</td><td>" 
+        + smartbin.hum + "</td><td>" 
+        + smartbin.topple + "</td></tr>")
+    })
+});
+
+// Save a new bin to the database, using the input in the form
+var submitSmartbin = function () {
+
+    var binid = $("#binid").val();
+    var filllevel = $("#filllevel").val();
+    var humidity = $("#humidity").val();
+    var temperature = $("#temperature").val();
+    var topple = $("#topple").val();
+    var timestamp = $("#timestamp").val();
+
+    // Push a new bin to the database using those values
+    smartbins.push({
+        "id": binid,
+        "filllevel": filllevel,
+        "hum": humidity,
+        "temp": temperature,
+        "topple": topple,
+        "timestamp": timestamp
+    });
+};
+
+// Get the single most recent bin from the database and
+// update the table with its values. This is called every time the child_added
+// event is triggered on the smartbin Firebase reference, which means
+// that this will update EVEN IF you don't refresh the page. Magic.
+smartbins.limitToLast(1).on('child_added', function (childSnapshot) {
+    // Get the bin data from the most recent snapshot of data
+    // added to the bin list in Firebase
+    smartbin = childSnapshot.val();
+
+    // Update the HTML to display the bin text
+    $("#binshow").html(smartbin.id)
+    $("#timeshow").html(smartbin.timestamp)
+    $("#fillshow").html(smartbin.filllevel)
+    $("#humshow").html(smartbin.hum)
+    $("#tempshow").html(smartbin.temp)
+    $("#toppleshow").html(smartbin.topple)
+});
+
+// When the window is fully loaded, call this function.
+// Note: because we are attaching an event listener to a particular HTML element
+// in this function, we can't do that until the HTML element in question has
+// been loaded. Otherwise, we're attaching our listener to nothing, and no code
+// will run when the submit button is clicked.
+$(window).load(function () {
+
+    // Find the HTML element with the id addData, and when the submit
+    // event is triggered on that element, call submitSmartbin.
+    $("#addData").submit(submitSmartbin);
+
+});
